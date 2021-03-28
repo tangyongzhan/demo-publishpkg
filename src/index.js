e914b4107642d5fa36e035f6387dd7bfc27a98a1
yongzhan <12421031+tangyongzhan@users.noreply.github.com>
2021-03-28 19:37:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import axios from 'axios'
-function getPost(type){
-    console.log('call function ')
-    return axios.get(`http://jsonplaceholder.typicode.com/${type}`).then(res => {
-        console.log(res)
-    })
+module.exports = {
+    printMsg: function(){
+        console.log('this message form package !!!')
+    }
 }
-
-export default getPost;
